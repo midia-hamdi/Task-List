a94217468ac7dc629ca3e2d320aa99e02f115d46
midia-hamdi <midiahamdi367@gmail.com>
2023-04-19 14:08:23 +0430

    main

---
+++ app.js
@@ -0,0 +1,19 @@
+const form = document.querySelector('#task-form');
+const taskInput = document.querySelector('#task');
+const filter = document.querySelector('#filter');
+const taskList = document.querySelector('.list-group');
+const clearBtn = document.querySelector('.clear-tasks');
+
+// LOAD ALL EVENT LISTENERS
+LoadEventListeners()
+
+// LOAD ALL EVENT LISTENERS
+function LoadEventListeners(){
+    // Add task event
+    form.addEventListener('submit', addTask);
+}
+
+// Add task 
+function addTask(e){
+    
+}
